bd30dc2f7551dc585fde0af8844d343b86808aef
KenLaw_ai <hklhfong01@gmail.com>
2020-05-11 14:25:37 +1000

    Added Plot graph

--- myQuack.py
+++ myQuack.py
@@ -16,7 +16,7 @@ You are welcome to use the pandas library if you know it.
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import plot_confusion_matrix
 
 import numpy as np
 import tensorflow as tf
@@ -27,17 +27,12 @@ from pandas import  pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-from sklearn.metrics import roc_curve, auc
-from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 from sklearn.preprocessing import LabelEncoder
-
-import seaborn as sns
-
 from sklearn.preprocessing import StandardScaler
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -80,6 +75,8 @@ def prepare_dataset(dataset_path):
     X = sc.fit_transform(X)
     #change type to string to make sure corrrect type
     y = dataset.iloc[:,1].astype(str)
+    #encode label result
+    y = encode(y)   
     print(X.shape)
     print(y.shape)
     return X,y
@@ -161,48 +158,34 @@ def build_NeuralNetwork_classifier(X_training, y_training):
     clf = keras.Model(inputs=inputs, outputs=outputs, name='TwoDense')
     clf.summary()
     clf.compile(loss='binary_crossentropy', optimizer='adam',metrics =['accuracy'])
-    clf.fit(X_training,y_training,epochs=100)
-    return clf
+    history = clf.fit(X_training,y_training,epochs=20,validation_split=0.2)
+    return clf,history
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-def plot_roc(y_test,y_score):
-    y_test = y_test.array
+def encode(y):
+    """
+    Encode the output result from y training and y testing set
+    deep learning neural network model is a model that only accept numeric type
+    on both input and output parameter.
+    In order to put the output in model,We need to transfrom data to encoded
+    lable.
     
-    
-    # Compute ROC curve and ROC area for each class
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(y_test.shape[0]):
-        fpr[i], tpr[i], thresholds  = roc_curve(y_test, y_score,pos_label='B')
-        roc_auc[i] = auc(fpr[i], tpr[i])
+    Parameters
+    ----------
+    y : Array of object
+        unprocessed tumour result.
 
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-    
-    plt.figure()
-    lw = 2
-    plt.plot(fpr[2], tpr[2], color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
-    plt.legend(loc="lower right")
-    plt.show()
-    
-    
-def encode(y_training,y_test):
+    Returns
+    -------
+    y : Array of int32
+        tumour result of training set (encoded in M = 0,B = 1).
+    """
     le = LabelEncoder()
-    le.fit(y_training)
+    le.fit(y)
     print(list(le.classes_))
-    y_training = le.transform(y_training)
-    y_test = le.transform(y_test)
-    return y_training,y_test,le
+    y = le.transform(y)
+    return y
     
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -235,14 +218,35 @@ if __name__ == "__main__":
     print('The accuracy of SVM:' + str(acc))
     
     #Display the NNC prediction accuracy
-    y_train,y_test,le = encode(y_train,y_test)
-    
-    clf = build_NeuralNetwork_classifier(x_train,y_train)
-    print(list(le.classes_))
-    #inverse transform the label
+    clf,history = build_NeuralNetwork_classifier(x_train,y_train)  
     test_loss, test_acc = clf.evaluate(x_test,  y_test, verbose=2)
-    print('\nTest accuracy:', test_acc)
+    print('The accuracy of NNC:', test_acc)
+    # plot training history
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
     
+    # disp = plot_confusion_matrix(history, x_test, y_test,
+    #                              cmap=plt.cm.Blues)
+    # disp.ax_.set_title('Confusion matrix')
+    # print(disp.confusion_matrix)
+    
+    
+    plt.show()
+
     
     
     
